5b891996247da8961a91a16f3e89706997014b89
SupriyaBhosaleGaikwad <supriya.1189gaikwad@gmail.com>
2022-08-27 23:44:24 -0400

    initial commit

---
+++ test/src/com/test/main/Sample.java
@@ -0,0 +1,18 @@
+package com.test.main;
+
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String[] args) {
+		int no = 0;
+		Scanner s = new Scanner(System.in);
+		no = s.nextInt();
+		if (no % 2 == 0) {
+			System.out.println("even");
+		} else {
+
+			System.out.println("even");
+		}
+	}
+
+}
